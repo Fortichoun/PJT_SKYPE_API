a6b4b66ec0b2887661f685e3013e2f435bb5086c
MissCaro <MissCaro>
2017-05-13 19:44:54 +0200

    Contacts requesting

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,13 +3,28 @@ const express = require('express');
 
 const router = express.Router();
 
-
+// GET on /api/contacts
+// Search for a user by email
 router.get('/', (req, res) => {
   User.findOne({ email: req.query.contactEmail })
         .exec((err, contact) => {
           res.json(contact);
         });
 });
+
+// GET on /api/contacts/allContacts
+// Give back to the front-end every contacts / friend requests received /
+// friend requests sent
+router.get('/allContacts', (req, res) => {
+  const contactsId = req.query.contacts.map(JSON.parse).map(contact => contact._id);
+  User.find({ _id: { $in: contactsId } })
+        .exec((err, contacts) => {
+          res.json(contacts);
+        });
+});
+
+// POST on api/contacts
+// Handle the friend requests sending
 router.post('/', (req, res, next) => {
   const { body } = req;
   User.findOne({ _id: body.userId })
